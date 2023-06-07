123c3e868e3b8083c45218d2229e67a8214408ff
Samuel Amity <sam.amity@gmail.com>
2023-06-07 12:00:19 -0400

    basic user routing added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,39 @@
 class UsersController < ApplicationController
+
+    def create
+        user = User.create(user_params)
+        render json: user, status: 201
+    end
+
+    def index
+        users = User.all
+        render json: users, include: :reviews, status: 200
+    end
+
+    def show
+        user = find_user
+        render json: user, status: 200
+    end
+
+    def update
+        user = find_user
+        user.update(user_params)
+        render json: user, status: 202
+    end
+
+    def destroy
+        user = find_user
+        user.destroy
+        head :no_content
+    end
+
+    private
+
+    def find_user
+        User.find_by(id: params[:id])
+    end
+
+    def user_params
+        params.permit(:name, :password, :age)
+    end
 end
